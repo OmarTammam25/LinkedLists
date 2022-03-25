eec9ebdb56b07604e9653b2f4de6f44e0fe9b888
OmarTammam25 <omartammam25@gmail.com>
2022-03-25 19:17:19 +0200

    Implemented single linked list successfully

--- src/SingleLinkedList.java
+++ src/SingleLinkedList.java
@@ -16,9 +16,9 @@ public class SingleLinkedList implements ILinkedList {
         // Reference to head node, made to iterate the list
         SLNode temp = head;
         int i=0; // iterator
-        if(index < 0) {
+        if(index < 0 || index > size()) {
             // if user inputs a negative index
-            // TODO output a message if user inputs a negative index.
+            System.out.println("Error");
         }else if(isEmpty()) {
             head = node;
             length++;
@@ -29,7 +29,7 @@ public class SingleLinkedList implements ILinkedList {
             length++;
         }else{
              // iterate until the index - 1
-            while(temp.getNext() != null && i < index) {
+            while(temp.getNext() != null && i < index-1) {
                 temp = temp.getNext();
                 i++;
             }
@@ -48,8 +48,8 @@ public class SingleLinkedList implements ILinkedList {
     public Object get(int index) {
         SLNode temp = head;
         int i = 0;
-        if(isEmpty()){
-            //TODO generate error
+        if(isEmpty() || index >= length || index < 0){
+            System.out.println("Error");
             return null;
         }else{
             while(temp.getNext()!=null && i < index){
@@ -65,7 +65,7 @@ public class SingleLinkedList implements ILinkedList {
         SLNode temp = head;
         int i = 0;
         if(isEmpty() || index<0 || index >= length){
-            //TODO generate error
+            System.out.println("Error");
         }else{
             while(temp.getNext() != null && i < index){
                 temp = temp.getNext();
@@ -88,22 +88,82 @@ public class SingleLinkedList implements ILinkedList {
 
     @Override
     public void remove(int index) {
-
+        SLNode current = head;
+        SLNode delete_node;
+        if(head == null || index >= length || index < 0) {
+            System.out.println("Error");
+        }else if(index == 0){ // delete from front
+            head = head.getNext();
+            current.setNext(null);
+            length--;
+        }else if(index == length-1){ // delete from tail
+            for (int i = 0; i < index-1; i++){
+                current = current.getNext();
+            }
+            current.setNext(null);
+            length--;
+        }else{ // delete from middle
+            // iterate through the list until index - 1 node
+            for (int i = 0; i < index-1; i++){
+                current = current.getNext();
+            }
+            delete_node = current.getNext();
+            current.setNext(delete_node.getNext()); // change the k-1 node to point to the node after the deleted one
+            delete_node.setNext(null);
+            length--;
+        }
     }
 
     @Override
     public int size() {
         //TODO delete this function. redundant.
+        if(isEmpty())
+            return 0;
         return length;
     }
 
     @Override
     public ILinkedList sublist(int fromIndex, int toIndex) {
-        return null;
+        SingleLinkedList myList = new SingleLinkedList();
+        SLNode temp = head;
+        // iterate until first node
+        if(head == null || fromIndex > toIndex || fromIndex < 0 || toIndex < 0 || toIndex >= length || fromIndex >= length){
+            System.out.println("Error");
+            return null;
+        }else {
+            for (int i = 0; i < fromIndex; i++) {
+                temp = temp.getNext();
+            }
+            //iterate until last index
+            for (int i = fromIndex; i <= toIndex; i++) {
+                myList.add(temp.getElement());
+                temp = temp.getNext();
+            }
+            return myList;
+        }
     }
 
     @Override
     public boolean contains(Object o) {
+        SLNode current = head;
+        for (int i =0; i < size(); i++) {
+            if(o == current.getElement()){
+                return true;
+            }
+            current = current.getNext();
+        }
         return false;
+    }
+
+    public void printList(SingleLinkedList myList){
+        Object temp;
+        if(myList == null){
+            System.out.println("Error");
+        }else{
+            for (int i = 0; i < myList.getLength(); i++) {
+                temp = myList.get(i);
+                System.out.println(temp);
+            }
+        }
     }
 }
--- src/driverCode.java
+++ src/driverCode.java
@@ -1,23 +1,101 @@
+import java.util.Scanner;
+
 public class driverCode {
     public static void main(String[] args) {
-        // driver code
+        // take input and put it in myList
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList myList = new SingleLinkedList();
+        String input = sc.nextLine().replaceAll("\\[|\\]", "");
+        String[] nums = input.split(", ");
 
-        myList.add(2, "omar");
-        myList.add("omar 2");
-        myList.add("omar and moustafa") ;
-        myList.set(2, "ana elly kan 3arfny");
-        myList.clear();
+        if(! nums[0].isEmpty()){
+            for(String i : nums){
+                myList.add(Integer.parseInt(i));
+            }
+        }
 
-        System.out.println("size of list = " + myList.getLength());
-        printList(myList);
+        String operation = sc.nextLine();
+        switch (operation){
+            case "add":
+                int index = sc.nextInt();
+                myList.add(index);
+                printListBrackets(myList);
+                break;
+            case "addToIndex":
+                int index2 = sc.nextInt();
+                int value = sc.nextInt();
+                myList.add(index2, value);
+                if(index2 >= 0 && index2 <= myList.size()){
+                    printListBrackets(myList);
+                }
+                break;
+            case "get":
+                Object input_get = myList.get(sc.nextInt());
+                if(input_get != null)
+                    System.out.println(input_get);
+                break;
+            case "set":
+                int set_index = sc.nextInt();
+                int set_value = sc.nextInt();
+                if(myList.isEmpty() || set_index<0 || set_index >= myList.getLength()){
+                    myList.set(set_index, set_value);
+                } else {
+                    myList.set(set_index, set_value);
+                    printListBrackets(myList);
+                }
+                break;
+            case "clear":
+                myList.clear();
+                printListBrackets(myList); // TODO see if this works
+                break;
+            case "isEmpty":
+                if(myList.isEmpty())
+                    System.out.println("True");
+                else
+                    System.out.println("False");
+                break;
+            case "remove":
+                int index_remove = sc.nextInt();
+                if(myList.isEmpty() || index_remove >= myList.getLength() || index_remove < 0) {
+                    System.out.println("Error");
+                    return;
+                }else{
+                    myList.remove(index_remove);
+                    printListBrackets(myList);
+                }
+                break;
+            case "sublist":
+                SingleLinkedList newList =(SingleLinkedList) myList.sublist(sc.nextInt(), sc.nextInt());
+                if(newList != null){
+                    printListBrackets(newList);
+                }
+                break;
+            case "contains":
+                if(myList.contains(sc.nextInt())){
+                    System.out.println("True");
+                }else{
+                    System.out.println("False");
+                }
+                break;
+            case "size":
+                System.out.println(myList.size());
+                break;
+        }
     }
-
-    public static void printList(SingleLinkedList myList){
+    public static void printListBrackets(SingleLinkedList myList){
         Object temp;
-        for (int i = 0; i < myList.getLength(); i++) {
-            temp = myList.get(i);
-            System.out.println(temp);
+        if(myList == null || myList.size() == 0){
+            System.out.print("[]");
+        }else{
+            System.out.print("[");
+            for (int i = 0; i < myList.getLength(); i++) {
+                temp = myList.get(i);
+                if(myList.getLength() - i == 1){
+                    System.out.print(temp + "]");
+                    break;
+                }
+                System.out.print(temp + ", ");
+            }
         }
 
     }
